4e31484fcfb3ae5e13865c1d5be88739262aa920
Krishna Pandian Dana Pandian <premadana@Krishnas-MacBook-Pro.local>
2025-05-29 23:18:01 +0530

    category and user table commision value migrations done

--- backend/controllers/auth/updateDetails.js
+++ backend/controllers/auth/updateDetails.js
@@ -3,7 +3,7 @@ import userModel from "../../models/userModel.js";
 // Update Details controller
 export const updateDetailsController = async (req, res) => {
     try {
-        const { newName, newEmail, newPhone, email } = req.body;
+        const { newName, newEmail, newPhone, email, newCommission } = req.body;
         const user = await userModel.findOne({ email });
         if (!user) {
             return res.status(401).send({
@@ -48,8 +48,19 @@ export const updateDetailsController = async (req, res) => {
                 message: "Mobile Number Updated Successfully!",
             });
         }
+        if (newCommission !== undefined) {
+            const response = await userModel.findOneAndUpdate(
+                { email: email },
+                { commission: newCommission },
+                { new: true }
+            );
+            res.status(200).send({
+                success: true,
+                message: "Commission Updated Successfully!",
+                commission: response.commission
+            });
+        }
 
-        //SUCCESS RESPONSE
     } catch (error) {
         console.log("Update Details Error: " + error);
         res.status(500).send({
